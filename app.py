71b6da8830b722305eb82d7cd179feb74487e006
Nabil Abbar <71392347+ABBARNABIL@users.noreply.github.com>
2022-09-14 00:13:48 +0200

    update app

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
 
 
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def visit():  # put application's code here
+    cur = conn.cursor()
+    cur.execute("SELECT count FROM visits")
+    count = cur.fetchone()
+    if count is None:
+        count = 0
+    else:
+        count = count[0]
+    cur.execute("UPDATE visits SET count = %s WHERE visit_id = 1", (count + 1,))
+    conn.commit()
+    return 'Hello World! I have been seen {} times.'.format(count)
 
 
 if __name__ == '__main__':
